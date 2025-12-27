45cf8a5a83e80c59eab5fc5b2d469ab19acf3489
yakuboff-m <m.yakuboff.15@gmail.com>
2025-12-27 22:57:32 +0900

    feat: create member schema model

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -4,5 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT_API ?? 3000);
+  console.log(`✅✅✅ Nestar-api ==> http://localhost:${process.env.PORT_API}`);
+  console.log(`✅✅✅ GraphQL doc ==> http://localhost:${process.env.PORT_API}/graphql`);
 }
 bootstrap();
