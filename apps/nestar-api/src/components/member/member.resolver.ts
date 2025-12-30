1d4b00e94ae8a00ee9a803e577b14a4a59eb1b27
yakuboff-m <m.yakuboff.15@gmail.com>
2025-12-30 23:07:35 +0900

    feat: develop signup business logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,35 +1,46 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { Member } from '../../libs/dto/member/member';
 
 @Resolver()
 export class MemberResolver {
-    constructor(private readonly memberService: MemberService) {}
+	constructor(private readonly memberService: MemberService) {}
 
-    @Mutation(() => String)
-    @UsePipes(ValidationPipe)
-    public async signup(@Args("input") input: MemberInput): Promise<string> {
-        console.log("Mutation: signup");
-        console.log("member.resolver -> signup api -> input:", input);
-        return this.memberService.signup();
-    }
+	@Mutation(() => Member)
+	@UsePipes(ValidationPipe)
+	public async signup(@Args('input') input: MemberInput): Promise<Member> {
+		try {
+			console.log('Mutation: signup');
+			// console.log('member.resolver -> signup api -> input:', input);
+			return this.memberService.signup(input);
+		} catch (err) {
+			console.log('Error, signup:', err);
+			throw new InternalServerErrorException(err);
+		}
+	}
 
-    @Mutation(() => String)
-    public async login(@Args("input") input: LoginInput): Promise<string> {
-        console.log("Mutation: login");
-        return this.memberService.login();
-    }
+	@Mutation(() => String)
+	public async login(@Args('input') input: LoginInput): Promise<String> {
+		try {
+			console.log('Mutation: login');
+			return this.memberService.login();
+		} catch (err) {
+			console.log('Error, login:', err);
+			throw new InternalServerErrorException(err);
+		}
+	}
 
-    @Mutation(() => String)
-    public async updateMember(): Promise<string> {
-        console.log("Mutation: updateMember");
-        return this.memberService.updateMember();
-    }
+	@Mutation(() => String)
+	public async updateMember(): Promise<string> {
+		console.log('Mutation: updateMember');
+		return this.memberService.updateMember();
+	}
 
-    @Query(() => String)
-    public async getMember(): Promise<string> {
-        console.log("Query: getMember");
-        return this.memberService.getMember();
-    }
+	@Query(() => String)
+	public async getMember(): Promise<string> {
+		console.log('Query: getMember');
+		return this.memberService.getMember();
+	}
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,25 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { MemberInput } from '../../libs/dto/member/member.input';
 
 @Injectable()
 export class MemberService {
+	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
 
-    constructor(@InjectModel("Member") private readonly memberModel: Model<null>) {}
-    
-    public async signup(): Promise<string>{
-        return "signup executed";
-    }
+	public async signup(input: MemberInput): Promise<Member> {
+		// TODO: Hash password
 
-    public async login(): Promise<string>{
-        return "login executed";
-    }
+		try {
+			const result = await this.memberModel.create(input);
+            // TODO: Authentication via token
+			return result;
+		} catch (err) {
+			console.log('Error, signup:', err);
+			throw new BadRequestException(err);
+		}
+	}
 
-    public async updateMember(): Promise<string>{
-        return "updateMember executed";
-    }
+	public async login(): Promise<string> {
+		return 'login executed';
+	}
 
-    public async getMember(): Promise<string>{
-        return "getMember executed";
-    }
+	public async updateMember(): Promise<string> {
+		return 'updateMember executed';
+	}
+
+	public async getMember(): Promise<string> {
+		return 'getMember executed';
+	}
 }
