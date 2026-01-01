8c6eaaa473c26e38e72d45dc5f9ab86bdf855487
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-01 23:35:33 +0900

    fix: integrate global pipe validation

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT_API ?? 3000);
   console.log(`✅✅✅ Nestar-api ==> http://localhost:${process.env.PORT_API}`);
   console.log(`✅✅✅ GraphQL doc ==> http://localhost:${process.env.PORT_API}/graphql`);
