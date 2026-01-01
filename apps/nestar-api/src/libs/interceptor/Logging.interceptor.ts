947be3a79d8408ef4b51c46988ec8d47a0c1123a
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-02 01:55:18 +0900

    feat: develop loggin via global interceptors

--- apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
@@ -1,18 +1,39 @@
-
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
+	private readonly logger: Logger = new Logger();
 
-    const now = Date.now();
-    return next
-      .handle()
-      .pipe(
-        tap(() => console.log(`After... ${Date.now() - now}ms`)),
-      );
-  }
+	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		const recordTime = Date.now(),
+			requestType = context.getType<GqlContextType>();
+		
+
+        if(requestType === 'http') {
+            // develop if needed
+        } else if(requestType === 'graphql') {
+            /** (1) Print Request **/
+            const gqlContext = GqlExecutionContext.create(context);
+            // console.log('gqlContext =>', gqlContext.getContext().req.body);
+            this.logger.log(`${this.stringify(gqlContext.getContext().req.body)}`, 'REQUEST');
+
+            /** (2) GraphQL handling Errors **/
+
+            /** (3) If no Errors -> giving Response below **/
+            return next.handle().pipe(
+                tap((context) => {
+                    const responseTIme = Date.now() - recordTime;
+                    this.logger.log(`${this.stringify(context)} - ${responseTIme}ms \n\n`, 'RESPONSE');
+                }),
+            );
+        }
+	}
+
+    private stringify(context: ExecutionContext): string {
+        // console.log(typeof context);
+        return JSON.stringify(context).slice(0, 75);
+    }
 }
--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor);
   await app.listen(process.env.PORT_API ?? 3000);
-  console.log(`✅✅✅ Nestar-api ==> http://localhost:${process.env.PORT_API}`);
-  console.log(`✅✅✅ GraphQL doc ==> http://localhost:${process.env.PORT_API}/graphql`);
+  logger.log(`✅✅✅  ==> http://localhost:${process.env.PORT_API}`, "Nestar-Api")
+  logger.log(`✅✅✅  ==> http://localhost:${process.env.PORT_API}/graphql`, "Playground")
+  // console.log(`✅✅✅ Nestar-api ==> http://localhost:${process.env.PORT_API}`);
+  // console.log(`✅✅✅ GraphQL doc ==> http://localhost:${process.env.PORT_API}/graphql`);
 }
 bootstrap();
