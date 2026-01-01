89844b02b29e57f5a8c7c2bb339d5d30f1d1a2a9
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-02 00:46:40 +0900

    feat: standard integration of logging interceptor

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor);
   await app.listen(process.env.PORT_API ?? 3000);
   console.log(`✅✅✅ Nestar-api ==> http://localhost:${process.env.PORT_API}`);
   console.log(`✅✅✅ GraphQL doc ==> http://localhost:${process.env.PORT_API}/graphql`);
