2160aea7c858a8667be7cf2a1ab91fc751e35214
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-04 08:57:33 +0900

    feat: create getAllMembersByAdmin & updateMemberByAdmin GraphQL APIs

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -21,6 +21,7 @@ export class MemberResolver {
 			return await this.memberService.login(input);
 	}
 
+	//Authenticated  
 	@Mutation(() => String)
 	public async updateMember(): Promise<string> {
 		console.log('Mutation: updateMember');
@@ -31,5 +32,21 @@ export class MemberResolver {
 	public async getMember(): Promise<string> {
 		console.log('Query: getMember');
 		return this.memberService.getMember();
+	}
+
+	/** ADMIN **/
+
+	//Authorization ADMIN
+	@Mutation(() => String)
+	public async getAllMembersByAdmin(): Promise<string> {
+		console.log('Query: getMember');
+		return this.memberService.getAllMembersByAdmin();
+	}
+
+	//Authorization ADMIN
+	@Mutation(() => String)
+	public async updateMemberByAdmin(): Promise<string> {
+		console.log('Query: getMember');
+		return this.memberService.updateMemberByAdmin();
 	}
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -55,4 +55,12 @@ export class MemberService {
 	public async getMember(): Promise<string> {
 		return 'getMember executed';
 	}
+
+	public async getAllMembersByAdmin(): Promise<string> {
+		return 'getAllMembersByAdmin executed';
+	}
+
+	public async updateMemberByAdmin(): Promise<string> {
+		return 'getAllMembersByAdmin executed';
+	}
 }
