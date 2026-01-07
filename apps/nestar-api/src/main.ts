8d8d8c87d7eeb654b350a9866b242ce88d07cf16
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-08 01:20:10 +0900

    fix: modify upload images configurations via graphqlUploaderExpress

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 import { GqlGlobalExceptionFilter } from './libs/filters/gql.filter';
+import {graphqlUploadExpress} from "graphql-upload";
+import * as express from "express";
+import { join } from 'path';
 
 async function bootstrap() {
   const logger = new Logger();
@@ -10,10 +13,13 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor);
   // app.useGlobalFilters(new GqlGlobalExceptionFilter());
+  app.enableCors({origin: true, credentials: true});
+
+  app.use(graphqlUploadExpress({maxFileSize: 15000000, max: 10}));
+  app.use('/uploads', express.static(join(process.cwd(), 'uploads')));
+
   await app.listen(process.env.PORT_API ?? 3000);
-  logger.verbose(`✅✅✅  ==> http://localhost:${process.env.PORT_API}`, "Nestar-Api")
-  logger.verbose(`✅✅✅  ==> http://localhost:${process.env.PORT_API}/graphql`, "Playground")
-  // console.log(`✅✅✅ Nestar-api ==> http://localhost:${process.env.PORT_API}`);
-  // console.log(`✅✅✅ GraphQL doc ==> http://localhost:${process.env.PORT_API}/graphql`);
+  logger.verbose(`✅✅✅  ==> http://localhost:${process.env.PORT_API}`, "Nestar-Api");
+  logger.verbose(`✅✅✅  ==> http://localhost:${process.env.PORT_API}/graphql`, "Playground");
 }
 bootstrap();
