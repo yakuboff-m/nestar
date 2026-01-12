0b3f900650c1e1efde2d305536c2c2d4ceaee48a
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-13 02:06:23 +0900

    feat: develop updateproperty business logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -10,31 +10,45 @@ import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
 
 @Resolver()
 export class PropertyResolver {
-    constructor(private readonly propertyService: PropertyService) {}
+	constructor(private readonly propertyService: PropertyService) {}
 
-    @Roles( MemberType.AGENT)
+	@Roles(MemberType.AGENT)
 	@UseGuards(AuthGuard)
-    @Mutation(()=> Property)
-    public async createProperty(
-        @Args('input') input: PropertyInput, 
-        @AuthMember("_id") memberId: ObjectId
-    ):Promise<Property>{
+	@Mutation(() => Property)
+	public async createProperty(
+		@Args('input') input: PropertyInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
 		console.log('Mutation: createProperty');
-        input.memberId = memberId; 
-        return await this.propertyService.createProperty(input);
-    }
+		input.memberId = memberId;
+		return await this.propertyService.createProperty(input);
+	}
 
-    @UseGuards(WithoutGuard)
-    @Query((returns) => Property)
-    public async getProperty(
-        @Args("propertyId") input: string,
-        @AuthMember("_id") memberId: ObjectId
-    ): Promise<Property> {
-        console.log('Query: getProperty');
-        const propertyId = shapeIntoMongoObjectId(input);
-        return await this.propertyService.getProperty(memberId, propertyId);
-    }
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Property)
+	public async getProperty(
+		@Args('propertyId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Query: getProperty');
+		const propertyId = shapeIntoMongoObjectId(input);
+		return await this.propertyService.getProperty(memberId, propertyId);
+	}
+
+	@Roles(MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Property)
+	public async updateProperty(
+		@Args('input') input: PropertyUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Mutation: updateProperty');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.propertyService.updateProperty(memberId, input);
+	}
 }
--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -9,6 +9,8 @@ import { ViewService } from '../view/view.service';
 import { PropertyStatus } from '../../libs/enums/property.enum';
 import { T, StatisticModifier } from '../../libs/types/common';
 import { ViewGroup } from '../../libs/enums/view.enum';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
+import * as moment from "moment";
 
 @Injectable()
 export class PropertyService {
@@ -76,5 +78,36 @@ export class PropertyService {
 			},
 		  )
 		  .exec();
+	  }
+
+	  public async updateProperty(memberId: ObjectId, input: PropertyUpdate): Promise<Property> {
+		let { propertyStatus, soldAt, deletedAt } = input;
+	  
+		const search: T = {
+		  _id: input._id,
+		  memberId,
+		  propertyStatus: PropertyStatus.ACTIVE,
+		};
+	  
+		if (propertyStatus === PropertyStatus.SOLD) soldAt = moment().toDate();
+		else if (propertyStatus === PropertyStatus.DELETE) deletedAt = moment().toDate();
+	  
+		const result = await this.propertyModel
+		  .findOneAndUpdate(search, input, {
+			new: true,
+		  })
+		  .exec();
+	  
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+	  
+		if (soldAt || deletedAt) {
+		  await this.memberService.memberStatsEditor({
+			_id: memberId,
+			targetKey: 'memberProperties',
+			modifier: -1,
+		  });
+		}
+	  
+		return result;
 	  }
 }
