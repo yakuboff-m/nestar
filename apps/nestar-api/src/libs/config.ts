68140d66c6eabb637e838bf76cf10aba6df393a4
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-15 01:02:57 +0900

    feat: develop admin related boardArticle GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -12,6 +12,7 @@ export const availablePropertySorts = [
   'propertyRank',
   'propertyPrice',
 ];
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
 
  // IMAGE CONFIGURATION (config.js)
  import { v4 as uuidv4 } from 'uuid';
