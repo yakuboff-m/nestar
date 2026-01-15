78b36e5bf64a6244e7eb331b1aed6958f3f48b61
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-16 00:31:07 +0900

    feat: createComment, updateComment, getComments GraphQL APIs

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -1,4 +1,48 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CommentService } from './comment.service';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { UseGuards } from '@nestjs/common';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 @Resolver()
-export class CommentResolver {}
+export class CommentResolver {
+	constructor(private readonly commentService: CommentService) {}
+
+	@UseGuards(AuthGuard)
+	@Mutation((returns) => Comment)
+	public async createComment(
+		@Args('input') input: CommentInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comment> {
+		console.log('Mutation: createComment');
+		return await this.commentService.createComment(memberId, input);
+	}
+
+	@UseGuards(AuthGuard)
+	@Mutation((returns) => Comment)
+	public async updateComment(
+		@Args('input') input: CommentUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comment> {
+		console.log('Mutation: updateComment');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.commentService.updateComment(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Comments)
+	public async getComments(
+		@Args('input') input: CommentsInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comments> {
+		console.log('Query: getComments');
+		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
+		return  await this.commentService.getComments(memberId, input);
+	}
+}
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -155,7 +155,7 @@ export class MemberService {
 	public async memberStatsEditor(input: StatisticModifier): Promise<Member> {
 		console.log("memberStatsEditor executed")
 		const { _id, targetKey, modifier } = input;
-		return await this.memberModel.findOneAndUpdate(
+		return await this.memberModel.findByIdAndUpdate(
 			_id, 
 			{ $inc: { [targetKey]: modifier } }, 
 			{ new: true }).exec();
