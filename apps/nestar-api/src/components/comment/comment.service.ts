27f394de2f2226af01a2cb9b3021127fe27ca249
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-16 00:39:34 +0900

    feat: develop removeCommentByAdmin graphQL api

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -83,33 +83,39 @@ export class CommentService {
 		return result;
 	}
 
-    public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
-        const { commentRefId } = input.search;
-        const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
-      
-        const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
-      
-        const result: Comments[] = await this.commentModel.aggregate([
-          { $match: match },
-          { $sort: sort },
-          {
-            $facet: {
-              list: [
-                { $skip: (input.page - 1) * input.limit },
-                { $limit: input.limit },
-                // meLiked
-                lookUpmember,
-                { $unwind: '$memberData' },
-              ],
-              metaCounter: [
-                { $count: 'total' },
-              ],
-            },
-          },
-        ]);
-      
-        if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
-      
-        return result[0];
-      }
+	public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
+		const { commentRefId } = input.search;
+		const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
+
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+
+		const result: Comments[] = await this.commentModel.aggregate([
+			{ $match: match },
+			{ $sort: sort },
+			{
+				$facet: {
+					list: [
+						{ $skip: (input.page - 1) * input.limit },
+						{ $limit: input.limit },
+						// meLiked
+						lookUpmember,
+						{ $unwind: '$memberData' },
+					],
+					metaCounter: [{ $count: 'total' }],
+				},
+			},
+		]);
+
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result[0];
+	}
+
+	//! ADMIN
+
+	public async removeCommentByAdmin(input: ObjectId): Promise<Comment> {
+		const result = await this.commentModel.findByIdAndDelete(input);
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+		return result;
+	}
 }
