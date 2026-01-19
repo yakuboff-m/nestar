7e0e38366c96657b97cbf25b63bf48ff5b47dd1d
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-20 07:53:18 +0900

    feat: intergration of lookupAuthMemberLiked complex query business logic

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -16,7 +16,7 @@ import { T, StatisticModifier } from '../../libs/types/common';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
 import * as moment from 'moment';
-import { lookUpmember, shapeIntoMongoObjectId, unwindMember } from '../../libs/config';
+import { lookupAuthMemberLiked, lookUpmember, shapeIntoMongoObjectId, unwindMember } from '../../libs/config';
 import { LikeService } from '../like/like.service';
 import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
@@ -144,6 +144,7 @@ export class PropertyService {
 							{ $skip: (input.page - 1) * input.limit },
 							{ $limit: input.limit },
 							// meLiked
+							lookupAuthMemberLiked(memberId),
 							lookUpmember,
 							unwindMember,
 						],
--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,4 +1,4 @@
-import {ObjectId} from "bson";
+import { ObjectId } from "bson";
 
 export const availableAgentSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews", "memberRank"]
 export const availableMemberSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews"]
@@ -15,50 +15,86 @@ export const availablePropertySorts = [
 export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
 export const availableCommentSorts = ['createdAt', 'updatedAt'];
 
- // IMAGE CONFIGURATION (config.js)
- import { v4 as uuidv4 } from 'uuid';
- import * as path from 'path';
- 
- export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
- export const getSerialForImage = (filename: string) => {
-     const ext = path.parse(filename).ext;
-     return uuidv4() + ext;
- };
+// IMAGE CONFIGURATION (config.js)
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+import { T } from "./types/common";
+import { pipeline } from "stream";
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+  const ext = path.parse(filename).ext;
+  return uuidv4() + ext;
+};
 
 export const shapeIntoMongoObjectId = (target: any) => {
-    return typeof target === "string" ? new ObjectId(target) : target;
-}; 
+  return typeof target === "string" ? new ObjectId(target) : target;
+};
+
+export const lookupAuthMemberLiked = (memberId: T, targetRefId: string = "$_id") => {
+  return {
+    $lookup: {
+      from: "likes",
+      let: {
+        localLikeRefId: targetRefId,
+        localMemberId: memberId,
+        localMyFavorite: true,
+      },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $and: [
+                { $eq: ["$likeRefId", "$$localLikeRefId"] },
+                { $eq: ["$memberId", "$$localMemberId"] },
+              ],
+            },
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            memberId: 1,
+            likeRefId: 1,
+            myFavorite: "$$localMyFavorite",
+          }
+        },
+      ],
+      as: "meLiked",
+    }
+  }
+};
 
 export const lookUpmember = {
-    $lookup: {
-        from: 'members',
-        localField: 'memberId',
-        foreignField: '_id',
-        as: 'memberData',
-    },
+  $lookup: {
+    from: 'members',
+    localField: 'memberId',
+    foreignField: '_id',
+    as: 'memberData',
+  },
 };
 
 export const unwindMember = {
-	$unwind: {
-		path: '$memberData',
-		preserveNullAndEmptyArrays: true, // This is important!
-	},
+  $unwind: {
+    path: '$memberData',
+    preserveNullAndEmptyArrays: true, // This is important!
+  },
 };
 
 export const lookupFollowingData = {
-    $lookup: {
-      from: 'members',
-      localField: 'followingId',
-      foreignField: '_id',
-      as: 'followingData',
-    },
-  };
-  
-  export const lookupFollowerData = {
-    $lookup: {
-      from: 'members',
-      localField: 'followerId',
-      foreignField: '_id',
-      as: 'followerData',
-    },
-  };
+  $lookup: {
+    from: 'members',
+    localField: 'followingId',
+    foreignField: '_id',
+    as: 'followingData',
+  },
+};
+
+export const lookupFollowerData = {
+  $lookup: {
+    from: 'members',
+    localField: 'followerId',
+    foreignField: '_id',
+    as: 'followerData',
+  },
+};
