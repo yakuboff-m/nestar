48ebd85ec3243c79ad4399074af6ad1526c18aed
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-22 19:14:10 +0900

    fix: modify batch module and connect to MongoDB

---
+++ apps/nestar-batch/src/batch.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { BatchController } from './batch.controller';
+import { BatchService } from './batch.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { ScheduleModule } from '@nestjs/schedule';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    ScheduleModule.forRoot()
+  ],
+  controllers: [BatchController],
+  providers: [BatchService],
+})
+export class BatchModule {}
