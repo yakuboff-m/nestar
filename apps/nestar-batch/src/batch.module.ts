a8eeb0d31bad66266008ff8c40753380419c584b
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-22 21:13:00 +0900

    fix: modify job scheduler business logic

--- apps/nestar-batch/src/batch.module.ts
+++ apps/nestar-batch/src/batch.module.ts
@@ -4,14 +4,17 @@ import { BatchService } from './batch.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { MongooseModule } from '@nestjs/mongoose';
+import PropertySchema from 'apps/nestar-api/src/schemas/Property.model';
+import MemberSchema from 'apps/nestar-api/src/schemas/Member.model';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot(),
     DatabaseModule,
-    ScheduleModule.forRoot()
+    ScheduleModule.forRoot(),
+    MongooseModule.forFeature([{name: "Property", schema: PropertySchema}]),
+    MongooseModule.forFeature([{name: "Member", schema: MemberSchema}])
   ],
   controllers: [BatchController],
   providers: [BatchService],
--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -1,20 +1,70 @@
+import { Member } from './../../nestar-api/src/libs/dto/member/member';
+import { Properties, Property } from './../../nestar-api/src/libs/dto/property/property';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { MemberStatus, MemberType } from 'apps/nestar-api/src/libs/enums/member.enum';
+import { PropertyStatus } from 'apps/nestar-api/src/libs/enums/property.enum';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class BatchService {
+  constructor(
+    @InjectModel("Property") private readonly propertyModel: Model<Property>,
+    @InjectModel("Member") private readonly memberModel: Model<Member>
+  ) {}
   
   public async batchRollback():Promise<void> {
     console.log("batchRollback");
+    await this.propertyModel.updateMany(
+      {
+        propertyStatus: PropertyStatus.ACTIVE,
+      },
+      { propertyRank: 0 },
+    )
+    .exec();
+
+    await this.memberModel
+      .updateMany(
+        {
+          memberStatus: MemberStatus.ACTIVE,
+          memberType: MemberType.AGENT,
+        },
+        { memberRank: 0 },
+      )
+      .exec();
   }
   
-  public async batchProperties():Promise<void> {
-    console.log("batchProperties");
+  public async batchTopProperties():Promise<void> {
+    console.log("batchTopProperties");
+    const properties: Property[] | null = await this.propertyModel.find({
+      propertyStatus: PropertyStatus.ACTIVE,
+      propertyRank: 0
+    }).exec();
+
+    const promisedList = properties.map(async (ele: Property) => {
+      const {_id, propertyLikes, propertyViews} = ele,
+       rank = propertyLikes * 2 + propertyViews * 1;
+      return await this.propertyModel.findByIdAndUpdate(_id, {propertyRank: rank});
+    });
+    await Promise.all(promisedList);
   }
   
-  public async batchAgents():Promise<void> {
-    console.log("batchAgents");
+  public async batchTopAgents():Promise<void> {
+    console.log("batchTopAgents");
+    const agents: Member[] = await this.memberModel.find({
+      memberType: MemberType.AGENT,
+      memberStatus: MemberStatus.ACTIVE,
+      memberRank: 0,
+    }).exec();
+
+    const promisedList = agents.map(async (ele: Member) => {
+      const { _id, memberProperties, memberArticles, memberLikes, memberViews} =  ele,
+        rank = memberProperties * 5 + memberArticles * 3 + memberLikes * 2 + memberViews * 1;
+      return await this.memberModel.findByIdAndUpdate(_id, {memberRank: rank}); 
+    });
+    await Promise.all(promisedList);
   }
-  
+
   getHello(): string {
     return 'Welcom to Nestar Batch Server!';
   }
